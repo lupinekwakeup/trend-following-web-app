e42062b701ba5f6b4a0b65ee6b377499b86e1512
nat <natnael08@protonmail.com>
2024-08-22 01:12:24 +0300

    fix redis connection

--- config.py
+++ config.py
@@ -9,5 +9,5 @@ class Config:
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY") or "your-jwt-secret-key"
-    CELERY_BROKER_URL = "redis://localhost:6379/0"
-    CELERY_RESULT_BACKEND = "redis://localhost:6379/0"
+    CELERY_BROKER_URL = os.getenv("REDISTOGO_URL", "redis://redis:6379")
+    CELERY_RESULT_BACKEND = os.getenv("REDISTOGO_URL", "redis://redis:6379")
